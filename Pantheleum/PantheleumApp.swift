3b43f60c42196665f214281ee1e45fdbb4445b53
hamza5645 <hamzaosama5645@outlook.com>
2024-08-26 01:13:36 +0300

    Initial Commit

---
+++ Pantheleum/PantheleumApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantheleumApp.swift
+//  Pantheleum
+//
+//  Created by Hamza Osama on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PantheleumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
